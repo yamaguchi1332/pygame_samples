f41f038f4f06f1faa5b998c1efa3ebc4b075f609
[GitHub <[korakoota2011]+[yamaguchi1332]@users.noreply.github.com>
2025-04-09 23:24:01 +0900

    README UPDEAT

--- demo_01.py
+++ demo_01.py
@@ -33,9 +33,9 @@ while running:
 
     # pygame.draw.circle(screen, color_on, (24 + x1 * 16, 24 + y1 * 16), 8)
     pygame.draw.rect(screen, color_on, Rect(24 + x1 * 16, 24 + y1 * 16, 12, 12))
-    x1 += 2
+    x1 += 150
     if x1 > 4:
-        x1 = 5
+        x1 = 0
 
     pygame.display.flip()  # update
     clock.tick(5)  # FPS, Frame Per Second
